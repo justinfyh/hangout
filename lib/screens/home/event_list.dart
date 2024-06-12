fcd3ba5c88cc1dc6bbd835c773ba0dd4b2da12aa
justin <justinhuang2020@gmail.com>
2024-06-12 15:23:34 +1200

    Only show user's associated events

--- lib/screens/home/event_list.dart
+++ lib/screens/home/event_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hangout/models/event.dart';
+import 'package:hangout/models/user.dart';
 import 'package:provider/provider.dart';
 
 class EventList extends StatefulWidget {
@@ -12,11 +13,16 @@ class EventList extends StatefulWidget {
 class _EventListState extends State<EventList> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserIdentity?>(context);
+    // final userData = Provider.of<UserModel?>(context);
+    final String uid = user!.uid;
     final events = Provider.of<List<Event>?>(context);
+    List<Event>? userEvents =
+        events?.where((event) => event.ownerUid == uid).toList();
 
-    if (events == null || events.isEmpty) {
+    if (userEvents == null || userEvents.isEmpty) {
       return const Center(
-        child: Text('No events available'),
+        child: Text('No userEvents available'),
       );
     }
 
@@ -24,27 +30,14 @@ class _EventListState extends State<EventList> {
       height: 250,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: events.length,
+        itemCount: userEvents.length,
         itemBuilder: (context, index) {
           return Container(
-            // width: 150,
-            margin: EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Expanded(
-                //   child: Container(
-                //     decoration: BoxDecoration(
-                //       borderRadius: BorderRadius.circular(8.0),
-                //       image: DecorationImage(
-                //         image: AssetImage(
-                //             'gs://hangout-ef87b.appspot.com/westfield-albany.jpg'), // Replace with your own images
-                //         fit: BoxFit.cover,
-                //       ),
-                //     ),
-                //   ),
-                // ),
-                SizedBox(height: 8),
+                const SizedBox(height: 8),
                 Container(
                   width: 160,
                   height: 160, // Adjust the size as needed
@@ -56,7 +49,7 @@ class _EventListState extends State<EventList> {
                     borderRadius:
                         BorderRadius.circular(10), // Same radius as above
                     child: Image.network(
-                      events[index].imageUrl,
+                      userEvents[index].imageUrl,
                       fit: BoxFit.fill,
                       loadingBuilder: (BuildContext context, Widget child,
                           ImageChunkEvent? loadingProgress) {
@@ -71,20 +64,20 @@ class _EventListState extends State<EventList> {
                         );
                       },
                       errorBuilder: (context, error, stackTrace) {
-                        return Center(
+                        return const Center(
                           child: Text('Error loading image'),
                         );
                       },
                     ),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 6,
                 ),
-                Text(events[index].name,
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                Text(events[index].location,
-                    style: TextStyle(color: Colors.grey)),
+                Text(userEvents[index].name,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                Text(userEvents[index].location,
+                    style: const TextStyle(color: Colors.grey)),
               ],
             ),
           );
