ef9dc44b8b6cebf4c6473f0a0f42a7e113dc4230
justin <justinhuang2020@gmail.com>
2024-07-20 23:37:36 +1200

    Display image for explore section

--- lib/screens/home/event_explore.dart
+++ lib/screens/home/event_explore.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:hangout/screens/event/event_page.dart';
 import 'package:hangout/models/event.dart';
 
 class ExploreSection extends StatelessWidget {
@@ -9,25 +9,67 @@ class ExploreSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(0.0, 10, 0, 0),
-          child: Text(
-            'Explore Other Events ${events.isNotEmpty ? events[0].name : ''}',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    final event = events[0];
+    return GestureDetector(
+      onTap: () {
+        // Navigate to event page passing eventid
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EventDetailsPage(
+              eventId: event.eventId,
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-          child: Text(
-            'See what\'s happening around you',
-            style: TextStyle(color: Colors.grey),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Text(
+              'Explore',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        // Add more widgets to display the list of events
-        // For example, you could use a ListView.builder to display each event
-      ],
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            child: Text(
+              'See what\'s happening around you',
+              style: TextStyle(color: Colors.grey, fontSize: 12),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              // Border color and width
+              borderRadius: BorderRadius.circular(15.0), // Border radius
+            ),
+            child: ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(15.0), // Border radius for image
+              child: Stack(
+                children: [
+                  Image.network(event.imageUrl),
+                  Positioned(
+                    top: 10, // Position the text at the top
+                    left: 10, // Position the text from the left
+                    child: Text(
+                      event.name,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        // backgroundColor: Colors.black.withOpacity(
+                        //     0.5), // Text background color with opacity
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
